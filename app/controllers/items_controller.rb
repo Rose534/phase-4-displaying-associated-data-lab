e8da19c0f8d5903f44b71d885a5cfb8490593d6c
rose534 <wambuirose534@gmail.com>
2023-03-17 19:54:00 +0300

    lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items.as_json(include: { user: { only: [:id, :username, :city] } })
+      end
 end
